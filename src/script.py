e9388e0f680c9d0d5d5317a5de1b8192a3bf4e22
Hemalatha0411 <140187972+Hemalatha0411@users.noreply.github.com>
2025-01-20 11:33:28 +0530

    Update script.py

--- src/script.py
+++ src/script.py
@@ -1,16 +1,1 @@
-import requests
-
-def fetch_data():
-    url = 'https://github.com/posts'
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        print("Successfully fetched data!")
-        posts = response.json()
-        for post in posts:
-            print(f"Post ID: {post['id']}, Title: {post['title']}")
-    else:
-        print("Failed to fetch data. Status code:", response.status_code)
-
-if __name__ == "__main__":
-    fetch_data()
+Print("Welcome to the Github Action usecase")
